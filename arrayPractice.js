63df2f3625996532ccaa6eb54b32fbd616cba75b
AlexASC4 <andro.torres16@gmail.com>
2017-07-12 11:09:34 -0400

    second commit

---
+++ arrayPractice.js
@@ -0,0 +1,6 @@
+var myArray = [3,"string",false];
+myArray.push(1);
+myArray.pop(3);
+myArray.length = 0
+myArray.length
+myArray.length;
---
+++ functions.js
@@ -0,0 +1,17 @@
+//function welloHorld(){
+    //return "Wello Horld";
+  //  console.log('wello holrd');
+//}
+//math.random(2)
+//This function returns a random number between 0 and num
+//function rand(num){
+   // var randnum = Math.random()*num;
+ //   var result = Math.floor(randnum);
+ //   return result;
+}
+//this is the same thing
+//return Math.floor(Math.random()*num);
+//console.log(rand(555)); //any number can replace 60
+var students = ["Rocket", "Jose", "Nic", "Devin", "Cyril"];
+students [rand(5)];
+console.log(students[rand(5)]);
